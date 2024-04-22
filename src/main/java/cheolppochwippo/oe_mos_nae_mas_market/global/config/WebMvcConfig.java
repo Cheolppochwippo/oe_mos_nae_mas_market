b47175c4cefc08ad466c5f8f586b08e640e1dde0
RebbitK <andxl9979@gmail.com>
2024-04-22 21:23:39 +0900

    :recycle: config

--- src/main/java/cheolppochwippo/oe_mos_nae_mas_market/global/config/WebMvcConfig.java
+++ src/main/java/cheolppochwippo/oe_mos_nae_mas_market/global/config/WebMvcConfig.java
@@ -2,18 +2,25 @@ package cheolppochwippo.oe_mos_nae_mas_market.global.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration // 스프링 빈으로 등록
 public class WebMvcConfig implements WebMvcConfigurer {
+
 	private final long MAX_AGE_SECS = 3600;
 
 
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("/resources/**").addResourceLocations("/resources/");
+	}
 
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
 
-		String[] allowedOrigins = { "https://fronts-two.vercel.app", "http://fronts-two.vercel.app","http://localhost:3000"};
+		String[] allowedOrigins = {"https://fronts-two.vercel.app", "http://fronts-two.vercel.app",
+			"http://localhost:3000"};
 		// 모든 경로에 대해
 		registry.addMapping("/**")
 			// Origin이 http:localhost:3000에 대해
