ebb6e3adba153bcab173c616826458ca67209d9f
RebbitK <andxl9979@gmail.com>
2024-05-03 17:21:40 +0900

    :recycle: elasticSearch 주석화

--- src/main/java/cheolppochwippo/oe_mos_nae_mas_market/domain/search/repository/ProductSearchRepository.java
+++ src/main/java/cheolppochwippo/oe_mos_nae_mas_market/domain/search/repository/ProductSearchRepository.java
@@ -4,8 +4,8 @@ import cheolppochwippo.oe_mos_nae_mas_market.domain.search.document.ProductDocum
 import java.util.List;
 import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
 
-public interface ProductSearchRepository extends ElasticsearchRepository<ProductDocument, String> {
-    List<ProductDocument> findByProductName(String keyword);
-
-    void deleteByProductId(Long productId);
-}
+//public interface ProductSearchRepository extends ElasticsearchRepository<ProductDocument, String> {
+////    List<ProductDocument> findByProductName(String keyword);
+////
+////    void deleteByProductId(Long productId);
+//}
--- src/main/java/cheolppochwippo/oe_mos_nae_mas_market/domain/search/service/SearchService.java
+++ src/main/java/cheolppochwippo/oe_mos_nae_mas_market/domain/search/service/SearchService.java
@@ -3,7 +3,7 @@ package cheolppochwippo.oe_mos_nae_mas_market.domain.search.service;
 import cheolppochwippo.oe_mos_nae_mas_market.domain.search.document.ProductDocument;
 import java.util.List;
 
-public interface SearchService {
-
-    List<ProductDocument> searchProduct(String keyword);
-}
+//public interface SearchService {
+//
+//    List<ProductDocument> searchProduct(String keyword);
+//}
